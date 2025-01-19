25038b14b34e23c018faf765aa9b18e4e5a392a6
CoolOmer <127267988+CoolOmer@users.noreply.github.com>
2025-01-18 22:31:30 -0500

    Update and rename steppr.ino (that was just a temporary name) to omer_stepultrasonicled_land.ino

--- omer_stepultrasonicled_land.ino
+++ omer_stepultrasonicled_land.ino
@@ -9,42 +9,44 @@ int trigPin = 11;    // TRIG pin
 int echoPin = 12;    // ECHO pin
 float duration_us, distance_cm;
 
-// For status LED config
+// For status LED configurations
 int fastBlue = 9;    // Blue LED pin
 int slowRed = 6;    // Red LED pin
 
-// Creates an instance of stepper class
-// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+// Pins entered in sequence ... IN1-IN3-IN2-IN4
 Stepper myStepper = Stepper(stepsPerRevolution, 2, 3, 4, 5);
 
 void setup() {
+  // Begins serial monitor, set up LEDs.
   Serial.begin(9600);
   pinMode(fastBlue, OUTPUT);
   pinMode(slowRed, OUTPUT);
   digitalWrite(fastBlue, LOW);
   digitalWrite(slowRed, LOW);
 
-  // Configure the trigger pin to output mode (US module)
+  // Configure the trigger pin to output mode (UltraSns module)
   pinMode(trigPin, OUTPUT);
-  // Configure the echo pin to input mode (US module)
+  // Configure the echo pin to input mode (UltraSns module)
   pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  // generate 10-microsecond pulse to TRIG pin
+    // Generate 10-microsecond pulse to TRIG pin
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
-    // measure duration of pulse from ECHO pin
+    // Measure duration of pulse from ECHO pin
     duration_us = pulseIn(echoPin, HIGH);
-    // calculate the distance
+    // Calculate the distance
     distance_cm = 0.017 * duration_us;
-    // print the value to Serial Monitor
+    // Print the value to Serial Monitor
     Serial.print("distance: ");
     Serial.print(distance_cm);
     Serial.println(" cm");
 
+    // Variables for motor speeds,
+    // based on rotations at top.
     int slowSpeed = 16;
     int fastSpeed = 55;
     int currentSpeed;
@@ -65,7 +67,7 @@ void loop() {
     Serial.print(currentSpeed);
     Serial.println();
 
-	// Rotate CCW
+	// Rotate counter-clock-wise
 	myStepper.step(-1*stepsPerRevolution);
 	delay(10);
 }
